b10b560a78380b6c92fefc6edf2e1ca6b33cab0e
ALFI <alfisyhrn24@gmail.com>
2023-10-31 18:34:00 +0700

    Add Theme.dart

---
+++ lib/shared.dart/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kBlackColor= Color(0x1E232C);
+Color kWhiteColor = Color(0xFFFFFF);
+Color kCyanColor = Color(0x35C2C1);
+
+TextStyle blackTextStyle = GoogleFonts.urbanist(
+  color: kBlackColor,
+);
