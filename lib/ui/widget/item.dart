71d52bbb2a47a17b1153fce79a5de674e0081bb3
adiyuda <adiyudapratama4@gmail.com>
2023-11-04 23:19:44 +0700

    fixing home page

--- lib/ui/widget/item.dart
+++ lib/ui/widget/item.dart
@@ -3,33 +3,50 @@ import 'package:flutter/material.dart';
 class Item extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final List<String> items = ['assets/tenda.png', 'assets/tenda.png', 'assets/tenda.png', 'assets/tenda.png', 'assets/tenda.png'];
-
-    return Container(
-      child: Expanded(
-        child: GridView.builder(
-          shrinkWrap: true,
-         // physics: NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 1.1, // Adjust the aspect ratio as needed
-          ),
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.grey[200],
-                child: Center(
-                  child: Text(
-                    items[index],
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
+    return Expanded(
+      child: GridView.builder(
+        shrinkWrap: true,
+        //physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.1, // Sesuaikan rasio aspek sesuai kebutuhan
+        ),
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Stack(children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey[200],
+                  image: DecorationImage(
+                      image: AssetImage('assets/tenda.png'), fit: BoxFit.cover),
                 ),
               ),
-            );
-          },
-        ),
+              // Container(
+              //   decoration: BoxDecoration(
+              //       gradient: LinearGradient(
+              //     begin: Alignment.bottomCenter,
+              //     end: Alignment.topCenter,
+              //     stops: [0.2, 1.0],
+              //     colors: [Colors.black, Colors.transparent],
+              //   ),
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      "Tenda",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    )),
+              )
+            ]),
+          );
+        },
       ),
     );
   }
