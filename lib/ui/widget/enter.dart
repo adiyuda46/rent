dce44123a76d3507f2ac49e9cda84c912e7ba1b5
adiyuda <adiyudapratama4@gmail.com>
2023-11-05 21:27:05 +0700

    widget enter

---
+++ lib/ui/widget/enter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Enter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 10,
+    );
+  }
+}
