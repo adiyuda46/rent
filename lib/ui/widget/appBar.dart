d172685d9ab0b88408ee90373b5651d50bbfb4a5
adiyuda <adiyudapratama4@gmail.com>
2023-11-05 20:17:37 +0700

    widget appbar

---
+++ lib/ui/widget/appBar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:rent/ui/widget/notifButton.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double height;
+  final TextEditingController textEditingController;
+
+  const CustomAppBar({
+    Key? key,
+    this.height = kToolbarHeight,
+    required this.textEditingController,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView( 
+      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.manual,
+      // Tambahkan SingleChildScrollView di sini
+      child: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height: 50,
+                  width: 250,
+                  child: TextField(
+                    controller: textEditingController,
+                    style: TextStyle(fontSize: 15), // Memperbesar ukuran teks input
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                      filled: true,
+                      fillColor: Colors.black12,
+                      hintStyle: TextStyle(fontSize: 15), // Memperbesar ukuran teks hint
+                      hintText: "Find your product",
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: 20,),
+              NotifButton(icon: Icons.notifications_none_rounded, onPressed: (){})
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
