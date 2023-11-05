e86d9ca8767b765652b77da341f43d4bd865e7fe
adiyuda <adiyudapratama4@gmail.com>
2023-11-05 21:11:16 +0700

    widget button

--- lib/ui/widget/item.dart
+++ lib/ui/widget/item.dart
@@ -5,8 +5,6 @@ class Item extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: GridView.builder(
-        shrinkWrap: true,
-        //physics: NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 1.1, // Sesuaikan rasio aspek sesuai kebutuhan
@@ -15,36 +13,29 @@ class Item extends StatelessWidget {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Stack(children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey[200],
-                  image: DecorationImage(
-                      image: AssetImage('assets/tenda.png'), fit: BoxFit.cover),
+            child: GestureDetector(
+              onTap: () => Navigator.pushNamed(context, '/DetailProduct'),
+              child: Stack(children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[200],
+                    image: DecorationImage(
+                        image: AssetImage('assets/tenda.png'), fit: BoxFit.cover),
+                  ),
                 ),
-              ),
-              // Container(
-              //   decoration: BoxDecoration(
-              //       gradient: LinearGradient(
-              //     begin: Alignment.bottomCenter,
-              //     end: Alignment.topCenter,
-              //     stops: [0.2, 1.0],
-              //     colors: [Colors.black, Colors.transparent],
-              //   ),
-              //   ),
-              // ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text(
-                      "Tenda",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    )),
-              )
-            ]),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Text(
+                        "Tenda",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      )),
+                )
+              ]),
+            ),
           );
         },
       ),
